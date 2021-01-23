4f8cd46ced9c5782730b4168c6c81c5327d28e92
nicolaydammer <nicolaydammer@gmail.com>
2021-01-23 03:50:46 +0100

    CHANGE: Change to test something

--- public/index.php
+++ public/index.php
@@ -7,10 +7,8 @@ use Slim\Psr7\Response;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
-
 $app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
-
 $app->run();
