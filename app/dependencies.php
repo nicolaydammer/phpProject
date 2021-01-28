847d2219902014e22367951084b3d1ae743f270a
nicolaydammer <nicolaydammer@gmail.com>
2021-01-28 21:38:53 +0100

    ADD: Added dependencies

--- app/dependencies.php
+++ app/dependencies.php
@@ -1,2 +1,10 @@
 <?php
 declare(strict_types=1);
+
+use DI\ContainerBuilder;
+
+return function (ContainerBuilder $containerBuilder) {
+    $containerBuilder->addDefinitions([
+        //todo: add dependencies for in the container.
+    ]);
+};
